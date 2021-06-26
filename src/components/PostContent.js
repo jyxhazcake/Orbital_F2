6061c3cc08e4bcffe4d41cb6f6b2371e09a8b46a
Jia Yixuan <jyxhazcake@yahoo.com>
2021-06-26 20:10:17 +0800

    feat: display posts from firestore

--- src/components/PostContent.js
+++ src/components/PostContent.js
@@ -1,28 +1,42 @@
 import React from "react";
 import logo from "./img/NUSlogo.png";
+import "firebase/firestore";
 
-const PostContent = () => {
+function PostContent(props) {
+  const {
+    name,
+    title,
+    durationstart,
+    durationend,
+    skills,
+    uid,
+    target,
+    photoURL,
+  } = props.post;
   return (
     <>
       <div className="flex bg-white shadow-md m-4 p-6 rounded">
         <div>
-          <img src={logo} alt="nuslogo" />
+          <img src={photoURL} alt="nuslogo" />
         </div>
         <div className="flex flex-col justify-between ml-4">
-          <h3 className="font-bold text-blue-900"> Organisation Name </h3>
-          <h2 className="font-bold text-xl"> Title of volunteer work </h2>
-          <p className="text-gray-700"> Duration: 01/01/20 - 12/12/21 </p>
+          <h3 className="font-bold text-blue-900"> {name} </h3>
+          <h2 className="font-bold text-xl"> {title} </h2>
+          <p className="text-gray-700">
+            {" "}
+            Duration: {durationstart} - {durationend}{" "}
+          </p>
         </div>
         <div className="flex items-center justify-center">
           {" "}
           <span className="text-blue-500 bg-blue-100 font-bold m-2 p-2 rounded">
             {" "}
-            Necessary Skills{" "}
+            {skills}{" "}
           </span>{" "}
         </div>{" "}
       </div>
     </>
   );
-};
+}
 
 export default PostContent;
--- src/pages/PagePosting.js
+++ src/pages/PagePosting.js
@@ -1,47 +1,25 @@
-import { useState, useEffect } from "react";
-import { firebase } from "@firebase/app";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import CreatePost from "../components/CreatePost";
 
 import PostBoard from "../components/PostBoard";
 import AppShell from "../components/AppShell";
 import PostContent from "../components/PostContent";
 
-
-/*    db.collection('posts').get().then((snapshot => {
-      snapshot.docs.forEach(doc => {
-        console.log(doc.data())
-      })
-    })) */
+const firestore = firebase.firestore();
 
 function PagePosting() {
-  // Task state has to be lifted to be at the App level
-  // because Header also needs to know the task state to display
-  // no. of undone tasks. It cannot be contained within TaskManager
-  // as child components cannot pass props to their parent components.
-  const [tasks, setTasksState] = useState([]);
+  const postsRef = firestore.collection("posts");
+  const query = postsRef.orderBy("createdAt").limit(25);
 
-  function setTasks(newTasks) {
-    setTasksState(newTasks);
-  }
-
-  useEffect(() => {
-    const uid = firebase.auth().currentUser?.uid;
-    const db = firebase.firestore();
-    const docRef = db.collection("/posts").doc(uid);
-
-    docRef.get().then((doc) => {
-      if (doc.exists) {
-        setTasksState(doc.data().posts);
-      } else {
-        setTasksState([]);
-      }
-    });
-  }, []);
+  const [posts] = useCollectionData(query, { idField: "id" });
 
   return (
     <>
       <AppShell />
-      <PostContent />
-      <PostBoard tasks={tasks} setTasks={setTasks} />
+      <CreatePost />
+      {posts && posts.map((pst) => <PostContent key={pst.id} post={pst} />)}
     </>
   );
 }
